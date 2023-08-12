4c2aab84f8110f5a019d008ab027ddadee0440c6
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-08-12 16:30:17 +0530

    Complete Q3

---
+++ Algo3.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public interface Algo3 {
+    public static void main(String[] args) {
+        int[] nums = {10,20,30,40,50};
+        int[] nums2=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums2[i]=nums[nums.length-1-i];
+        }
+        nums=nums2;
+        System.out.print("[");
+        for (int i = 0; i < nums2.length; i++) {
+            System.out.print(nums[i]+", ");
+        }
+        System.out.println("\b]");
+    }
+}
